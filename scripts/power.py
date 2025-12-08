e233dc7aa56c512e3e1a09e3e47a744bbdda99f7
Radley E. Sidwell-Lewis <radley.lewis@proton.me>
2025-12-08 20:26:59 +0700

    refactor: rofi power menu

--- scripts/power.py
+++ scripts/power.py
@@ -12,12 +12,14 @@ logout_icon = "󰗽"
 yes_icon = ""
 no_icon = ""
 
-config_dir = os.environ.get("XDG_CONFIG_HOME", "") + "/rofi"
+CONFIG_DIR = os.environ.get("XDG_CONFIG_HOME", "")
+LOCKSCREEN_WALLPAPER = f"{CONFIG_DIR}/qtile/assets/wallpapers/lock_screen.png"
 
 
 def get_uptime() -> str:
     result = subprocess.run(["uptime", "-p"], capture_output=True, text=True)
     return result.stdout.strip().replace("up ", "")
+
 
 def main_menu() -> str:
     options: List[str] = [
@@ -38,7 +40,7 @@ def main_menu() -> str:
             "-mesg",
             f"Uptime: {uptime}",
             "-theme",
-            f"{config_dir}/power.rasi",
+            f"{CONFIG_DIR}/rofi/common.rasi",
         ],
         input=menu,
         capture_output=True,
@@ -54,7 +56,7 @@ def confirm() -> bool:
         [
             "rofi",
             "-theme",
-            f"{config_dir}/power.rasi",
+            f"{CONFIG_DIR}/rofi/common.rasi",
             "-theme-str",
             "listview {columns: 2; lines: 1;}",
             "-dmenu",
@@ -70,39 +72,20 @@ def confirm() -> bool:
     return result == yes_icon
 
 
-def lock_qtile():
-    subprocess.Popen(
-        [
-            "swaylock",
-            "-i",
-            "~/.config/qtile/assets/wallpapers/lock_screen.png",
-            "--clock",
-        ]
-    )
-
-
-def sleep():
-    script_path = os.path.expanduser("~/.config/qtile/scripts/sleep.sh")
-    subprocess.Popen([script_path])
-
-
 def power():
     selected = main_menu()
 
-    if selected == lock_icon:
-        lock_qtile()
-    elif selected == sleep_icon:
-        sleep()
-    else:
-        action_map = {
-            shutdown_icon: ["systemctl", "poweroff"],
-            reboot_icon: ["systemctl", "reboot"],
-            logout_icon: ["pkill", "-KILL", "-u", os.environ["USER"]],
-        }
+    action_map = {
+        lock_icon: ["swaylock", "-i", LOCKSCREEN_WALLPAPER],
+        sleep_icon: [os.path.expanduser("~/.config/qtile/scripts/sleep.sh")],
+        shutdown_icon: ["systemctl", "poweroff"],
+        reboot_icon: ["systemctl", "reboot"],
+        logout_icon: ["pkill", "-KILL", "-u", os.environ["USER"]],
+    }
 
-        if action := action_map[selected]:
-            if confirm():
-                subprocess.run(action)
+    if action := action_map[selected]:
+        if confirm():
+            subprocess.run(action)
 
 
 if __name__ == "__main__":
