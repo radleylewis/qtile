f4c58604432c9fe1ad7f217832329deb9fcab7dd
Radley E. Sidwell-Lewis <radley.lewis@proton.me>
2026-05-24 21:53:29 +0700

    fix: gestures, dim on idle

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -23,12 +23,16 @@ fi
 # gestures
 libinput-gestures-setup start
 
+# desktop backend
+xdg-desktop-portal &
+
 # idle/lock
 dim_timeout=120  # 2 minutes to dim
 lock_timeout=300 # 5 minutes to lock
 BRIGHTNESS_FILE="/tmp/prev_brightness"
 swayidle -w \
-	timeout $dim_timeout "brightnessctl g > $BRIGHTNESS_FILE && brightnessctl set 10" \
-	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set $(cat $BRIGHTNESS_FILE)" \
+	timeout $dim_timeout "brightnessctl g > $BRIGHTNESS_FILE && brightnessctl set 10%" \
+	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set \$(cat $BRIGHTNESS_FILE)" \
 	timeout $lock_timeout "$HOME/.config/qtile/scripts/sleep.sh" \
-	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set $(cat $BRIGHTNESS_FILE)" &
+	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set \$(cat $BRIGHTNESS_FILE)" &
+resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set $(cat $BRIGHTNESS_FILE)" &
