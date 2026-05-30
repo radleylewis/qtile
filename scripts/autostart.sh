8b8b2e28e04b424fb2446696a3c0654f60b363fe
Radley E. Sidwell-Lewis <radley.lewis@proton.me>
2026-05-30 15:49:20 +0700

    fix: only dim if brightness > dim

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -24,14 +24,18 @@ fi
 libinput-gestures-setup start
 
 # desktop backend
+xdg-desktop-portal-wlr &
+sleep 1
 xdg-desktop-portal &
 
 # idle/lock
 dim_timeout=120  # 2 minutes to dim
 lock_timeout=300 # 5 minutes to lock
 BRIGHTNESS_FILE="/tmp/prev_brightness"
+DIM_VALUE=10
+
 swayidle -w \
-	timeout $dim_timeout "brightnessctl g > $BRIGHTNESS_FILE && brightnessctl set 10%" \
+	timeout $dim_timeout "current=\$(brightnessctl g); max=\$(brightnessctl m); percent=\$((current * 100 / max)); if [ \$percent -gt $DIM_VALUE ]; then brightnessctl g > $BRIGHTNESS_FILE && brightnessctl set ${DIM_VALUE}%; fi" \
 	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set \$(cat $BRIGHTNESS_FILE)" \
 	timeout $lock_timeout "$HOME/.config/qtile/scripts/sleep.sh" \
 	resume "[ -f $BRIGHTNESS_FILE ] && brightnessctl set \$(cat $BRIGHTNESS_FILE)" &
--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 if pactl list sink-inputs | grep -q 'state: RUNNING'; then
-	exit 0 # Audio is playing — do not lock
+	exit 0
 else
 	swaylock -i ~/.config/qtile/assets/wallpapers/lock_screen.jpg &
 	sleep 2
-	loginctl suspend
+	echo "$(date): calling zzz" >>/tmp/sleep.log
+	sudo zzz
+	echo "$(date): resumed from zzz" >>/tmp/sleep.log
 fi
