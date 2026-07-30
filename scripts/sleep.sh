e1d2bd6d10145389b5d399a71e045ef0db7fb0ad
Radley E. Sidwell-Lewis <radley.lewis@proton.me>
2026-07-30 07:13:30 +0700

    chore: sleep logs

--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -4,8 +4,6 @@ if pactl list sink-inputs | grep -q 'state: RUNNING'; then
 	exit 0
 else
 	swaylock -i ~/.config/qtile/assets/wallpapers/lock_screen.jpg &
-	sleep 2
-	echo "$(date): calling zzz" >>/tmp/sleep.log
+	sleep 60
 	sudo zzz
-	echo "$(date): resumed from zzz" >>/tmp/sleep.log
 fi
